cbe031d463c9738098aed542c4131f266be9901c
Luiz Fernando <luiz.lol14@gmail.com>
2022-09-28 23:49:28 -0300

    Implementado

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone celular1 = new Iphone("84 99415006617", "iPhone 14", "4222212472316540154", 512);
+celular1.Ligar();
+celular1.ReceberLigacao();
+celular1.InstalarAplicativo("Whatsapp");
+
+Nokia celular2 = new Nokia(" 84 996929042", "Ultima geração", "456156424212125415641223", 1024);
+celular2.Ligar();
+celular2.ReceberLigacao();
+celular2.InstalarAplicativo("Instagram");
